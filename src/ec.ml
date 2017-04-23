139b4a58148225731530774c7649b5df34105563
Cafe au Lait <cafeaulaits@me.com>
2017-04-24 01:38:14 +0900

    add tests of ElGamal public key encryption on elliptic curves.

--- src/ec.ml
+++ src/ec.ml
@@ -5,152 +5,151 @@ type 't solution =
     NONE | SINGLE of 't | DOUBLE of 't * 't
 
 (** This module indicates an elliptic curve {i y^2 = x^3 + ax + b}. *)
-module type Spec =
-    sig
-      type element
-      val a : element
-      val b : element
-    end
+module type Spec = sig
+  type element
+
+  val a : element
+  val b : element
+end
 
 (** This functor defines elliptic curve S on field F, and basic operations on it. *)
-module Make (F : Field.t) (S : Spec with type element = F.element) =
-    struct
-      open F
-      open S
+module Make (F : Field.t) (S : Spec with type element = F.element) = struct
+  open F
+  open S
 
-      let n4 = of_int 4
-      let n27 = of_int 27
+  let n4 = of_int 4
+  let n27 = of_int 27
 
-      (** Unit element. We use projective coordinates to represent a point. *)
-      let o = ( zero, one, zero )
+  (** Unit element. We use projective coordinates to represent a point. *)
+  let o = ( zero, one, zero )
 	  
-      (** Check if the equations has distinct roots. *)
-      let check_parameter() =
-	  let d = add (mul n4 (mul a (square a))) (mul n27 (square b)) in
-	  assert (not (equal d zero));
-	  ()
+  (** Check if the equations has distinct roots. *)
+  let check_parameter() =
+      let d = add (mul n4 (mul a (square a))) (mul n27 (square b)) in
+      assert (not (equal d zero));
+      ()
 
-      (** Transform a projective coordinate to an affine coordinate. *)
-      let to_affine_coord( x, y, z ) =
-	  (* assume ( x, y, z ) != o *)
-	  if equal z one then
-	      ( x, y )
-	  else begin
-	      let z = invert z in
-	      ( (mul x z), (mul y z) )
-	  end
+  (** Transform a projective coordinate to an affine coordinate. *)
+  let to_affine_coord( x, y, z ) =
+      (* assume ( x, y, z ) != o *)
+      if equal z one then
+	  ( x, y )
+      else begin
+	  let z = invert z in
+	  ( (mul x z), (mul y z) )
+      end
 
-      (** Transform an affine coordinate to a projective coordinate. *)
-      let to_projective_coord( x, y ) =
+  (** Transform an affine coordinate to a projective coordinate. *)
+  let to_projective_coord( x, y ) =
+      ( x, y, one )
+
+  (** Convert a point to a string. *)
+  let string_of_point( x, y, z ) =
+      if equal z zero then
+	  "o"
+      else begin
+	  let x, y = to_affine_coord( x, y, z ) in
+	  "("^(to_string x)^","^(to_string y)^")"
+      end
+
+  (** Inversion. *)
+  let unary_minus( x, y, z ) =
+      if equal z zero then
+	  o
+      else begin
+	  let z = invert z in
+	  let x = mul x z in
+	  let y = mul y z in
+	  let y = unary_minus y in
 	  ( x, y, one )
+      end
 
-      (** Convert a point to a string. *)
-      let string_of_point( x, y, z ) =
-          if equal z zero then
-	      "o"
-          else begin
-	      let x, y = to_affine_coord( x, y, z ) in
-	      "("^(to_string x)^","^(to_string y)^")"
-          end
+  let counter1 = ref 0
+  let counter2 = ref 0
+  let counter3 = ref 0
 
-      (** Inversion. *)
-      let unary_minus( x, y, z ) =
-	  if equal z zero then
-	      o
-	  else begin
-	      let z = invert z in
-	      let x = mul x z in
-	      let y = mul y z in
-	      let y = unary_minus y in
-	      ( x, y, one )
-	  end
+  let report() =
+      Printf.printf "ec: the numbers of addition operations P + Q, (P = Q) %d, (P = -Q) %d, (P != Q) %d\n"
+          (!counter1) (!counter2) (!counter3);
+      ()
 
-      let counter1 = ref 0
-      let counter2 = ref 0
-      let counter3 = ref 0
-
-      let report() =
-	  Printf.printf "ec: the numbers of addition operations P + Q, (P = Q) %d, (P = -Q) %d, (P != Q) %d\n"
-              (!counter1) (!counter2) (!counter3);
-	  ()
-
-      (** Addition. *)
-      let plus ( x1, y1, z1 ) ( x2, y2, z2 ) =
-	  if equal z1 zero then
-	      ( x2, y2, z2 )
-	  else if equal z2 zero then
-	      ( x1, y1, z1 )
-	  else begin
-	      let x1z2 = mul x1 z2 in
-	      let x2z1 = mul x2 z1 in
-	      let u = sub x1z2 x2z1 in
-	      let v = sub (mul y1 z2) (mul y2 z1) in
-	      if equal u zero then begin
-		  if equal v zero then begin
-		      incr counter1;
-		      (* P = Q *)
-		      let x = x1 in
-		      let y = y1 in
-		      let z = z1 in
-		      let x2 = square x in
-		      let y2 = square y in
-		      let z2 = square z in
-		      let xy = mul x y in
-		      let _2xy = shift_left xy 1 in
-		      let _4xy = shift_left _2xy 1 in
-		      let _2x2 = shift_left x2 1 in
-		      let _2y2 = shift_left y2 1 in
-		      let yz = mul y z in
-		      let r = add (add _2x2 x2) (mul a z2) in
-		      let t = shift_left yz 1 in
-		      let s = sub (square r) (mul _4xy t) in
-		      let t2 = square t in
-		      if equal t zero then
-			  o
-		      else
-			  ( (mul s t), (sub (mul (sub (mul _2xy t) s) r) (mul _2y2 t2)), (mul t2 t) )
-		  end
-		  else begin
-		    (* P = -Q *)
-		      incr counter2;
-		      assert (equal (add (mul y1 z2) (mul y2 z1)) zero);
+  (** Addition. *)
+  let plus ( x1, y1, z1 ) ( x2, y2, z2 ) =
+      if equal z1 zero then
+	  ( x2, y2, z2 )
+      else if equal z2 zero then
+	  ( x1, y1, z1 )
+      else begin
+	  let x1z2 = mul x1 z2 in
+	  let x2z1 = mul x2 z1 in
+	  let u = sub x1z2 x2z1 in
+	  let v = sub (mul y1 z2) (mul y2 z1) in
+	  if equal u zero then begin
+	      if equal v zero then begin
+		  incr counter1;
+		  (* P = Q *)
+		  let x = x1 in
+		  let y = y1 in
+		  let z = z1 in
+		  let x2 = square x in
+		  let y2 = square y in
+		  let z2 = square z in
+		  let xy = mul x y in
+		  let _2xy = shift_left xy 1 in
+		  let _4xy = shift_left _2xy 1 in
+		  let _2x2 = shift_left x2 1 in
+		  let _2y2 = shift_left y2 1 in
+		  let yz = mul y z in
+		  let r = add (add _2x2 x2) (mul a z2) in
+		  let t = shift_left yz 1 in
+		  let s = sub (square r) (mul _4xy t) in
+		  let t2 = square t in
+		  if equal t zero then
 		      o
-		  end
+		  else
+		      ( (mul s t), (sub (mul (sub (mul _2xy t) s) r) (mul _2y2 t2)), (mul t2 t) )
 	      end
 	      else begin
-		  incr counter3;
-		  (* P != Q *)
-		  let u2 = square u in
-		  let v2 = square v in
-		  let z1z2 = mul z1 z2 in
-		  let z1z2u2 = mul z1z2 u2 in
-		  let w = sub (mul z1z2 v2) (mul (add x1z2 x2z1) u2) in
-		  let y3 = sub (F.unary_minus (mul v w)) (mul z1z2u2 (sub (mul x1 y2) (mul x2 y1))) in
-		  ( (mul u w), y3, (mul z1z2u2 u) )
+		  (* P = -Q *)
+		  incr counter2;
+		  assert (equal (add (mul y1 z2) (mul y2 z1)) zero);
+		  o
 	      end
 	  end
-      
-      (** Multiplication. *)
-      let multiply n p =
-	  let q = unary_minus p in
-	  mass_add n plus p o q
+	  else begin
+	      incr counter3;
+	      (* P != Q *)
+	      let u2 = square u in
+	      let v2 = square v in
+	      let z1z2 = mul z1 z2 in
+	      let z1z2u2 = mul z1z2 u2 in
+	      let w = sub (mul z1z2 v2) (mul (add x1z2 x2z1) u2) in
+	      let y3 = sub (F.unary_minus (mul v w)) (mul z1z2u2 (sub (mul x1 y2) (mul x2 y1))) in
+	      ( (mul u w), y3, (mul z1z2u2 u) )
+	  end
+      end
 
-      (** Compute {i x^3 + ax + b}. *)
-      let value x =
-	  let x2 = square x in
-	  add (mul x x2) (add (mul a x) b)
+  (** Multiplication. *)
+  let multiply n p =
+      let q = unary_minus p in
+      mass_add n plus p o q
 
-      (** Given {i x}, find the solutions {i y} of {i y^2 = x^3 + ax + b}. *)
-      let find_y x =
-	  let v = value x in
-	  match legendre_symbol v with
-	      1 ->
-		let y1 = quadratic_residue v in
-		let y2 = F.unary_minus y1 in
-		if mod2 y1 = 0 then
-		    DOUBLE( y1, y2 )
-		else
-		    DOUBLE( y2, y1 )
-	    | 0 -> SINGLE (quadratic_residue v)
-	    | _ -> NONE
-    end
+  (** Compute {i x^3 + ax + b}. *)
+  let value x =
+      let x2 = square x in
+      add (mul x x2) (add (mul a x) b)
+
+  (** Given {i x}, find the solutions {i y} of {i y^2 = x^3 + ax + b}. *)
+  let find_y x =
+      let v = value x in
+      match legendre_symbol v with
+	  1 ->
+	    let y1 = quadratic_residue v in
+	    let y2 = F.unary_minus y1 in
+	    if mod2 y1 = 0 then
+		DOUBLE( y1, y2 )
+	    else
+		DOUBLE( y2, y1 )
+	| 0 -> SINGLE (quadratic_residue v)
+	| _ -> NONE
+end
--- src/test.ml
+++ src/test.ml
@@ -227,31 +227,18 @@ let test_Pocklington_Lehmer2() =
 
 module F1009 = F_big_int.Make (struct type element = Z.t let p = big_int_of_int 1009 end)
 
-module ECSpec1009 =
-    struct
-      type element = Z.t
+module ECSpec1009 = struct
+  type element = Z.t
 
-      let a = big_int_of_int 71
-      let b = big_int_of_int 602
-    end
+  let a = big_int_of_int 71
+  let b = big_int_of_int 602
+end
 
 module E1009 = Ec.Make (F1009) (ECSpec1009)
 
-let print_point( x, y, z ) =
-    if F1009.equal z F1009.zero then
-	print_string "o"
-    else begin
-	let x, y = E1009.to_affine_coord( x, y, z ) in
-	print_string "(";
-	print_string (F1009.to_string x);
-	print_string ",";
-	print_string (F1009.to_string y);
-	print_string ")"
-    end
-
 let test_ec_ff() =
     let make_point x y =
-	( (big_int_of_int x), (big_int_of_int y), F1009.one )
+	( (F1009.of_int x), (F1009.of_int y), F1009.one )
     in
     let get_point( x, y, z ) =
 	let x, y = E1009.to_affine_coord( x, y, z ) in
@@ -287,92 +274,64 @@ let test_ec_ff() =
     E1009.report();
     ()
 
-(*
-open Arith
+module F22BIT = F_big_int.Make (struct type element = Z.t let p = big_int_of_int 4063417 end)
 
-module PrimintOp = Field.MakeGenericOperation (Primint)
+module ECSpec22BIT = struct
+  type element = Z.t
 
-module Varint2Op = Field.MakeGenericOperation (Varint2)
-
-module Varint10Op = Field.MakeGenericOperation (Varint10)
-
-module Varint2Op2 = Varint.MakeOperation (Varint2)
-
-(*
-module FP997 = Primintmod.Make (struct let p = 997 end)
-
-module FB997 = Varint2mod.Make (struct let p = "997" end)
-
-module F22BIT = Varint2mod.Make (struct let p = "4063417" end)
-
-module ECSpec22BIT =
-    struct
-      type element = Varint2.element
-
-      let a = Varint2.of_int 83527
-      let b = Varint2.of_int 42987
-    end
+  let a = big_int_of_int 83527
+  let b = big_int_of_int 42987
+end
 
 module E22BIT = Ec.Make (F22BIT) (ECSpec22BIT)
-
-let print_point ( x, y, z ) =
-    if Varint2.equal z Varint2.zero then
-	print_string "o"
-    else begin
-	let x, y = E22BIT.to_affine_coord( x, y, z ) in
-	print_string "(";
-	print_string (Conv.string_of_varint2 x);
-	print_string ",";
-	print_string (Conv.string_of_varint2 y);
-	print_string ")"
-    end
 
 let test_ElGamal22() =
     let n = 4063417 in
     let make_point x y =
-	( (Varint2.of_int x), (Varint2.of_int y), Varint2.one )
+	( (F22BIT.of_int x), (F22BIT.of_int y), F22BIT.one )
     in
     let plus p q =
 	E22BIT.plus p q
     in
     let multiply n p =
-	E22BIT.multiply (Varint2.of_int n) p
+	E22BIT.multiply (big_int_of_int n) p
     in
     let equal_point p q =
 	let x1, y1 = E22BIT.to_affine_coord p in
 	let x2, y2 = E22BIT.to_affine_coord q in
-	Varint2.equal x1 x2 && Varint2.equal y1 y2
+	F22BIT.equal x1 x2 && F22BIT.equal y1 y2
     in
     let find_random_point() =
 	let rec continue x =
-	    let w = Varint2.of_int x in
+	    let w = F22BIT.of_int x in
 	    match E22BIT.find_y w with
-		Ec.SINGLE y | Ec.DOUBLE( y, _ ) -> ( w, y, Varint2.one )
+		Ec.SINGLE y | Ec.DOUBLE( y, _ ) -> ( w, y, F22BIT.one )
 	      |	Ec.NONE -> continue ((x + 1) mod n)
 	in
 	continue (Random.int n)
     in
     let g = make_point 0 2785936 in
     Printf.printf "F_%d\n" n;
-    print_string "E: y^2 = x^3 + 83527x + 42987\n";
-    print_string "g = "; print_point g; print_newline();
-    print_string "[4061081]g = "; print_point (multiply 4061081 g); print_newline();
+    print_string "EC: y^2 = x^3 + 83527x + 42987\n";
+    Printf.printf "g = %s\n" (E22BIT.string_of_point g);
+    Printf.printf "[4061081]g = %s\n" (E22BIT.string_of_point (multiply 4061081 g));
     for i = 0 to 1000 do
 	let x = 3 + Random.int 100000 in
 	let k = 1 + Random.int (n - 1) in
 	let h = multiply x g in
 	let m = find_random_point() in
 	Printf.printf "secret key x = %d\n" x;
-	print_string "public key h = "; print_point h; print_newline();
+        Printf.printf "public key h = %s\n" (E22BIT.string_of_point h);
 	Printf.printf "k = %d\n" k;
-	print_string "message m = "; print_point m; print_newline();
+        Printf.printf "message m = %s\n" (E22BIT.string_of_point m);
 	let a = multiply k g in
 	let b = plus (multiply k h) m in
-	print_string "encrypted message = "; print_point a; print_string ", "; print_point b; print_newline();
+        Printf.printf "encrypted message = %s, %s\n"
+            (E22BIT.string_of_point a) (E22BIT.string_of_point b);
 	let c = E22BIT.unary_minus a in
 	let c = multiply x c in
 	let d = plus b c in
-	print_string "decoded message = "; print_point d; print_newline();
+        Printf.printf "decoded message = %s\n" (E22BIT.string_of_point d);
 	if equal_point m d then
 	    print_string "ok\n"
 	else
@@ -380,244 +339,77 @@ let test_ElGamal22() =
     done;
     ()
 
-*)
+module P192BIT = struct
+  type element = Z.t
 
-module P192BIT = 
-    struct
-      (* 2^192 - 2^64 - 1 *)
-      let p = "6277101735386680763835789423207666416083908700390324961279"
-	      
-    end
+  (* 2^192 - 2^64 - 1 *)
+  let p = big_int_of_string "6277101735386680763835789423207666416083908700390324961279"
+end
 
-module F192BIT = Varint2mod.Make (P192BIT)
+module F192BIT = F_big_int.Make (P192BIT)
 
-module ECSpec192BIT =
-    struct
-      type element = Varint2.element
+module ECSpec192BIT = struct
+  type element = Z.t
 
-      let a =
-	  Conv.varint2_of_string "6277101735386680763835789423207666416083908700390324961276"
+  let a = big_int_of_string "6277101735386680763835789423207666416083908700390324961276"
+  let b = big_int_of_string "2455155546008943817740293915197451784769108058161191238065"
 
-      let b =
-	  Conv.varint2_of_string "2455155546008943817740293915197451784769108058161191238065"
-
-      let order =
-	  Conv.varint2_of_string "6277101735386680763835789423176059013767194773182842284081"
-
-    end
+  let order = big_int_of_string "6277101735386680763835789423176059013767194773182842284081"
+end
 
 module E192BIT = Ec.Make (F192BIT) (ECSpec192BIT)
-
-let string_of_affine_point ( x, y, z ) =
-    if Varint2.equal z Varint2.zero then
-	"o"
-    else begin
-	let x, y = E192BIT.to_affine_coord( x, y, z ) in
-	"("^(Conv.string_of_varint2 x)^","^(Conv.string_of_varint2 y)^")"
-    end
-
-let string_of_projective_point ( x, y, z ) =
-    "("^(Conv.string_of_varint2 x)^","^(Conv.string_of_varint2 y)^","^(Conv.string_of_varint2 z)^")"
 
 let test_ElGamal192() =
     let plus p q =
 	E192BIT.plus p q
     in
     let multiply n p =
-	E192BIT.multiply (Varint2.of_int n) p
+	E192BIT.multiply (big_int_of_int n) p
     in
     let equal_point p q =
 	let x1, y1 = E192BIT.to_affine_coord p in
 	let x2, y2 = E192BIT.to_affine_coord q in
-	Varint2.equal x1 x2 && Varint2.equal y1 y2
+	F192BIT.equal x1 x2 && F192BIT.equal y1 y2
     in
     let find_random_point() =
 	let rec continue x =
-	    let w = Varint2.of_int x in
+	    let w = F192BIT.of_int x in
 	    match E192BIT.find_y w with
-		Ec.SINGLE y | Ec.DOUBLE( y, _ ) -> ( w, y, Varint2.one )
+		Ec.SINGLE y | Ec.DOUBLE( y, _ ) -> ( w, y, F192BIT.one )
 	      |	Ec.NONE -> continue (x + 1)
 	in
 	continue (Random.int 100000)
     in
-    let x =
-	Conv.varint2_of_string "602046282375688656758213480587526111916698976636884684818"
-    in
-    let y =
-	Conv.varint2_of_string "174050332293622031404857552280219410364023488927386650641"
-    in
-    let g = ( x, y, Varint2.one ) in
+    let x = F192BIT.of_string "602046282375688656758213480587526111916698976636884684818" in
+    let y = F192BIT.of_string "174050332293622031404857552280219410364023488927386650641" in
+    let g = ( x, y, F192BIT.one ) in
     print_string "F_(2^192 - 2^64 - 1)\n";
-    Printf.printf "p = %s\n" (Conv.string_of_varint2 F192BIT.p);
-    Printf.printf "g = %s\n" (string_of_projective_point g);
-(*
-    let g2 = plus g g in
-    Printf.printf "g + g = %s\n" (string_of_projective_point g2);
-    if true then
-	exit 0;
-*)
-    let order =
-	Conv.varint2_of_string "6277101735386680763835789423176059013767194773182842284081"
-    in
-    Printf.printf "[-]g = %s\n" (string_of_affine_point (E192BIT.multiply order g));
-    for i = 0 to 10 do
+    Printf.printf "p = %s\n" (F192BIT.to_string P192BIT.p);
+    Printf.printf "g = %s\n" (E192BIT.string_of_point g);
+    Printf.printf "[-]g = %s\n" (E192BIT.string_of_point (E192BIT.multiply ECSpec192BIT.order g));
+    for i = 0 to 1000 do
 	let x = 3 + Random.int 100000 in
 	let k = 1 + Random.int 100000 in
 	let h = multiply x g in
 	let m = find_random_point() in
 	Printf.printf "secret key x = %d\n" x;
-	Printf.printf "public key h = %s\n" (string_of_affine_point h);
+	Printf.printf "public key h = %s\n" (E192BIT.string_of_point h);
 	Printf.printf "k = %d\n" k;
-	Printf.printf "message m = %s\n" (string_of_affine_point m);
+	Printf.printf "message m = %s\n" (E192BIT.string_of_point m);
 	let a = multiply k g in
 	let b = plus (multiply k h) m in
-	Printf.printf "encrypted message = %s,%s\n" (string_of_affine_point a)
-	    (string_of_affine_point b);
+	Printf.printf "encrypted message = %s,%s\n" (E192BIT.string_of_point a)
+	    (E192BIT.string_of_point b);
 	let c = E192BIT.unary_minus a in
 	let c = multiply x c in
 	let d = plus b c in
-	Printf.printf "decoded message = %s\n" (string_of_affine_point d);
+	Printf.printf "decoded message = %s\n" (E192BIT.string_of_point d);
 	if equal_point m d then
 	    print_string "ok\n"
 	else
 	    assert false
     done;
     ()
-
-(*
-
-
-module E1 = Ec.Make (Varint2)
-
-let test_ec_plus() =
-    let make_point( x, y ) =
-	(Varint2.of_int x), (Varint2.of_int y), Varint2.one
-    in
-    let print_point( x, y, z ) =
-	let x = Varint2.to_int x in
-	let y = Varint2.to_int y in
-	let z = Varint2.to_int z in
-	print_string "(";
-	print_int (x / z);
-	print_string ",";
-	print_int (y / z);
-	print_string ")"
-    in
-    let test a b l =
-	let p = Array.of_list (List.map make_point l) in
-	let length = Array.length p in
-	for i = 0 to length - 1 do
-	    for j = 0 to length - 1 do
-		print_point p.(i);
-		print_string " + ";
-		print_point p.(j);
-		print_string " = ";
-		let p3 = E1.plus a b p.(i) p.(j) in
-		if p3 = E1.o then
-		    print_string "o"
-		else
-		    print_point p3;
-		print_newline()
-	    done
-	done
-    in
-    (* y^2 = x^3 + 1. Z/6Z *)
-    let a = Varint2.zero in
-    let b = Varint2.one in
-    E1.check_parameter a b;
-    let l = [(0,1); (0,-1); (2,3); (2,-3); (-1,0)] in
-    print_string "y^2 = x^3 + 1:\n";
-    test a b l;
-    (* y^2 = x^3 + 4x. Z/4Z *)
-    let a = Varint2.of_int 4 in
-    let b = Varint2.zero in
-    E1.check_parameter a b;
-    let l = [(0,0); (2,4); (2,-4)] in
-    print_string "\ny^2 = x^3 + 4x:\n";
-    test a b l;
-    ()
-    
-let test_ec192() =
-    let vi2_of_string s =
-	Conv.decimal_to_binary32 (Varint10.of_string s)
-    in
-    let p = vi2_of_string "6277101735386680763835789423207666416083908700390324961279" in
-    let a = vi2_of_string "6277101735386680763835789423207666416083908700390324961276" in
-    let b = vi2_of_string "2455155546008943817740293915197451784769108058161191238065" in
-    let x = vi2_of_string "602046282375688656758213480587526111916698976636884684818" in
-    let y = vi2_of_string "174050332293622031404857552280219410364023488927386650641" in
-    let order = vi2_of_string "6277101735386680763835789423176059013767194773182842284081" in
-    let e = Ec.make_ec (Varint2mod.make) p a b in
-    let g = ( x, y, Varint2.one ) in
-    let print_point p = print_point e p in
-    let v = e.Ec.multiply order g in
-    print_string "[order]g = ";
-    print_point v;
-    print_newline();
-    let blank_area = ref 0 in
-    let max_blank_area = ref 0 in
-    for x = 100000 to 100000 + 40000 do
-	let x = Varint2.of_int x in
-	let v = e.Ec.value x in
-	if Varint2Op.legendre_symbol v p >= 0 then begin
-	    blank_area := 0
-	end
-	else begin
-	    incr blank_area;
-	    if !blank_area > !max_blank_area then
-		max_blank_area := !blank_area
-	end
-    done;
-    Printf.printf "max blank area size = %d\n" (!max_blank_area);
-    ()
-*)
-
-let main() =
-    let seed = int_of_float (Unix.time()) in
-    Random.init seed;
-    Printf.printf "seed = %d\n" seed;
-
-(*
-    let x =
-	Conv.varint2_of_string "348100664587244062809715104560438820728046977854773301282"
-    in
-(*
-    let y =
-	Conv.varint2_of_string "174050332293622031404857552280219410364023488927386650641"
-    in
-*)
-    let x = { x with Varint.aval = Array.sub x.Varint.aval 0 2 } in
-(*
-    let y = { y with Varint.aval = Array.sub y.Varint.aval 3 2 } in
-*)
-    let z = Varint2.square x in
-    Printf.printf "z = %s\n" (Conv.string_of_varint2 z);
-    let x10 = Conv.binary_to_decimal x in
-(*    let y10 = Conv.binary_to_decimal y in *)
-    let z10 = Varint10.square x10 in
-    let z2 = Conv.decimal_to_binary z10 in
-    if Varint2.equal z z2 then
-	print_string "z is correct\n"
-    else
-	print_string "z is wrong\n";
-
-    Printf.printf "x = %s\n" (Varint2.to_string x);
-(*    Printf.printf "y = %s\n" (Varint2.to_string y); *)
-    Printf.printf "z = %s\n" (Varint2.to_string z);
-    Printf.printf "z = %s (correct)\n" (Varint2.to_string z2);
-    if true then
-	exit 1;
-*)
-(*
-    test_ec_ff();
-    test_ec_plus();
-    test_ec_order();
-    test_ec192();
-    test_ElGamal22();
-*)
-    test_ElGamal192();
-    ()
-*)
 
 let main() =
     let seed = int_of_float (Unix.time()) in
@@ -640,6 +432,8 @@ let main() =
     test_Pocklington_Lehmer();
     test_Pocklington_Lehmer2();
     test_ec_ff();
+    test_ElGamal22();
+    test_ElGamal192();
     ()
 
 ;;
